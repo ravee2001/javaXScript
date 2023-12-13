5dfaa630a5216d26d67587f41e86943b4a5661ee
ravee2001 <s6406021620076@email.kmutnb.ac.th>
2023-12-13 08:48:29 +0700

    methodArray

---
+++ chapter3/createArray.js
@@ -0,0 +1,11 @@
+let arrayLangth = 7;
+let arr1 = [];
+let arr2 = Array(arrayLangth);
+let arr3 = ['A',false,5]
+
+//Getting array length
+console.log(arr1.length);
+console.log(arr2.length);
+console.log(arr3.length);
+console.log(arr3[0]);
+console.log(arr3[2]);
